55dbb91849ee4bc13f057bb34c15f033148ae60f
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-08-05 14:26:17 +0600

    Project setup done

---
+++ src/components/shared/navbar/Navbar.items.ts
@@ -0,0 +1,27 @@
+export const navbarItems = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Shop",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "About",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    link: "#",
+  },
+  {
+    id: 5,
+    title: "Checkout",
+    link: "#",
+  },
+];
